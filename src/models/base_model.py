bf16493d89942e77e6d8205e89e4c3944eb87597
Sergiusz <gusevxsergey@gmail.com>
2022-11-29 01:31:05 +0100

    transferred the majority of the modelling pipeline to the Python package

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,4 +1,8 @@
 import joblib
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 from loguru import logger
 
 # import of local modules
@@ -45,14 +49,76 @@ class LogReg:
 
         return pred_dict[y_pred]
 
-    def _load_data():
-        pass
+    def _load_data(self, df_path: str) -> pd.DataFrame:
+        df = pd.read_csv(
+            self.config["data_input"]["all_data"],
+            encoding="ISO-8859-1",
+            names=["Sentiment", "News Headline"],
+        )
+        return df
 
-    def _process_data():
-        pass
+    def _decode_target(self, df: pd.DataFrame) -> pd.DataFrame:
+        keys = ["neutral", "positive", "negative"]
+        vals = [0, 1, 2]
+        map_dict = dict(zip(keys, vals))
+
+        df.Sentiment = df.Sentiment.map(map_dict)
+        return df
+
+    def _split_data(self, df: pd.DataFrame) -> tuple:
+        """
+        @return: tuple(pd.DataFrame)
+        """
+        X_train, X_test, y_train, y_test = train_test_split(
+            df["News Headline"],
+            df.Sentiment,
+            test_size=self.config["model_input"]["test_size"],
+            random_state=self.config["model_input"]["rnd_seed"],
+            shuffle=True,
+            stratify=df.Sentiment,
+        )
+        return (X_train, X_test, y_train, y_test)
+
+    def _tf_idf(self, X_train: pd.DataFrame, X_test: pd.DataFrame) -> tuple:
+        """
+        @return: tuple(*), where * is like List[List[int]],
+            but scipy.sparse._csr.csr_matrix
+        """
+        vectorizer = TfidfVectorizer(
+            stop_words="english",
+            ngram_range=self.config["vectorizer_input"]["ngram_range"],
+            lowercase=True,
+            max_features=self.config["vectorizer_input"]["max_features"],
+        )
+
+        X_train_vec = vectorizer.fit_transform(X_train)
+        X_test_vec = vectorizer.transform(X_test)
+
+        return X_train_vec, X_test_vec
+
+    def _process_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df
 
     def _fit_vectorizer():
+        # Look at _tf_idf function - don't duplicate
         pass
+
+    def fit_model(self, X_train_vec, y_train) -> None:
+        logreg = LogisticRegression(
+            C=self.config["model_input"]["regularization_strength"],
+            solver="lbfgs",
+            multi_class="multinomial",
+            class_weight=self.config["model_input"]["class_weight"],
+            random_state=self.config["model_input"]["rnd_seed"],
+            n_jobs=-1,
+        )
+        logger.info("train model .. start")
+
+        logreg.fit(X_train_vec, y_train)
+        self.model = logreg
+
+        logger.info("train model .. done")
+        return None
 
     def save_vectorizer(self, vectorizer_path: str) -> None:
         """
@@ -61,14 +127,8 @@ class LogReg:
         @return: None
         """
         joblib.dump(self.vectorizer, vectorizer_path)
-        logger.info("Vectorizer saving has been done")
+        logger.info("save vectorizer .. done")
         return None
-
-    def _split_data():
-        pass
-
-    def fit_model():
-        pass
 
     def save_model(self, model_path: str) -> None:
         """
@@ -77,7 +137,7 @@ class LogReg:
         @return: None
         """
         joblib.dump(self.model, model_path)
-        logger.info("Model saving has been done")
+        logger.info("save model .. done")
         return None
 
 
